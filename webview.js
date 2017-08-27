557d44bfafdcab5de80b478bd05756e439bb9ae8
Kan Fushihara <kan.fushihara@gmail.com>
2017-08-27 17:10:04 +0900

    タブが切り替わったらall threadへ移動する
    - 今いるチャンネルの未読を取れなくなることへの対策

--- webview.js
+++ webview.js
@@ -1,4 +1,8 @@
 module.exports = (Franz, options) => {
+    $(window).on('blur', function(e) {
+        $('button.p-channel_sidebar__link--all-threads').trigger('click');
+    });
+
     function getMessages() {
         var unread = $('.p-channel_sidebar__channel--unread').length;
         var reply  = $('.p-channel_sidebar__channel--im.p-channel_sidebar__channel--unread').length;
